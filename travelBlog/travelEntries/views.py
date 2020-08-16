d3fe94bb9ef73b5edd8f581cd54797af348d93b0
Fabliha Hossain <fablihahossain27@gmail.com>
2020-08-16 18:57:31 -0400

    tested accessing data in views

--- travelBlog/travelEntries/views.py
+++ travelBlog/travelEntries/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import User, Entry, EntryImage
 
 def index(request):
-	return render(request, 'index.html')
+	# Getting all users
+	users = User.objects.all()
+
+	# Getting all entries
+	entries = Entry.objects.all()
+
+	# Getting all entry images
+	images = EntryImage.objects.all()
+
+	return render(request, 'index.html', {'users':users, 'entries':entries, 'images':images})
