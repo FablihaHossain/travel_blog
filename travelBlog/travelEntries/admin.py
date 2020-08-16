fdd04533e3da8916c98ec23645b10cbfe75defcb
Fabliha Hossain <fablihahossain27@gmail.com>
2020-08-16 16:27:19 -0400

    updated admin functionality to view, update, and modify objects in db

--- travelBlog/travelEntries/admin.py
+++ travelBlog/travelEntries/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Entry, EntryImage
+from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+	list_display = ['name', 'username', 'email', 'role']
+
+class ArrayModelAdmin(admin.ModelAdmin, DynamicArrayMixin):
+    list_display = ['title', 'author', 'descriptions']
+admin.site.register(Entry, ArrayModelAdmin)
+
+@admin.register(EntryImage)
+class EntryImages(admin.ModelAdmin):
+	list_display = ['entry', 'image']
+
+# Credit to https://github.com/gradam/django-better-admin-arrayfield
