cba2e9dadf2ba076303472cce2ddd670261bc9b1
Fabliha Hossain <fablihahossain27@gmail.com>
2020-08-16 19:32:48 -0400

    updated models with comments

--- travelBlog/travelEntries/models.py
+++ travelBlog/travelEntries/models.py
@@ -2,41 +2,42 @@ from django.db import models
 #from django.contrib.postgres.fields import ArrayField
 from django_better_admin_arrayfield.models.fields import ArrayField
 
+# User Model 
 class User(models.Model):
-	ROLES = [('G', 'General User'), ('A', 'Admin User')]
+	ROLES = [('G', 'General User'), ('A', 'Admin User')] # Currently only two roles
 	name = models.CharField(max_length=100, blank = False)
 	username = models.CharField(max_length=100, blank = False)
 	email = models.EmailField(blank = False)
 	password = models.CharField(max_length = 300, blank = False)
 	role = models.CharField(max_length = 20, choices = ROLES, blank = False)
 
-	# Print statement for Python Shell
+	# Print statement for Python Shell and Admin Functionality
 	def __str__(self):
 		return self.username
 
+# Entry Model
 class Entry(models.Model):
 	title = models.CharField(max_length = 400, blank = False)
+	# Each entry belongs to one user in the database
 	author = models.ForeignKey('User', on_delete=models.CASCADE)
 	descriptions = ArrayField(models.CharField(max_length = 500), size = 5)
 
-	# Print statement for Python Shell	
+	# Print statement for Python Shell and Admin Functionality
 	def __str__(self):
 		return self.title
 
+# Sub model that allows for uploads of image files
 class EntryImage(models.Model):
+	# Each image uploaded must pertain to a particular entry in the database
 	entry = models.ForeignKey('Entry', on_delete=models.CASCADE)
-	# Attempting to create a new path file for each entry
-	#pathname = 'entryImages/%s' % (entry.name)
-	
 
+	# Defines pathname for uploaded images, each entry will have their own directory
 	def get_upload_path(instance, filename):
 		return 'entryImages/entry_{0}/{1}'.format(instance.entry.id, filename)
 
+	# Image source
 	image = models.ImageField(upload_to = get_upload_path)
-
-	# Print statement for Python Shell	
-	# def __str__(self):
-	# 	return self.entry
 
 # Credit to https://docs.djangoproject.com/en/3.1/ref/contrib/postgres/fields/#arrayfield
 # Credit to https://github.com/gradam/django-better-admin-arrayfield
+# Credit to https://docs.djangoproject.com/en/3.1/ref/models/fields/#django.db.models.FileField.upload_to
