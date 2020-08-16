b223f122364d6e1eb8d4586848f2e5888f1a031e
Fabliha Hossain <fablihahossain27@gmail.com>
2020-08-16 16:26:30 -0400

    modified models to add a new model of entryImage for each entry

--- travelBlog/travelEntries/models.py
+++ travelBlog/travelEntries/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+#from django.contrib.postgres.fields import ArrayField
+from django_better_admin_arrayfield.models.fields import ArrayField
 
 class User(models.Model):
 	ROLES = [('G', 'General User'), ('A', 'Admin User')]
@@ -17,11 +18,20 @@ class Entry(models.Model):
 	title = models.CharField(max_length = 400, blank = False)
 	author = models.ForeignKey('User', on_delete=models.CASCADE)
 	descriptions = ArrayField(models.CharField(max_length = 500), size = 5)
-	# Attempting to create a new path file for each entry
-	pathname = 'entryImages/%s/%s' % (author, title)
-	images = ArrayField(models.ImageField(upload_to = pathname), size = 5)
 
 	# Print statement for Python Shell	
 	def __str__(self):
 		return self.title
+
+class EntryImage(models.Model):
+	entry = models.ForeignKey('Entry', on_delete=models.CASCADE)
+	# Attempting to create a new path file for each entry
+	pathname = 'entryImages/%s' % (entry)
+	image = models.ImageField(upload_to = pathname)
+
+	# Print statement for Python Shell	
+	def __str__(self):
+		return self.entry
+
 # Credit to https://docs.djangoproject.com/en/3.1/ref/contrib/postgres/fields/#arrayfield
+# Credit to https://github.com/gradam/django-better-admin-arrayfield
