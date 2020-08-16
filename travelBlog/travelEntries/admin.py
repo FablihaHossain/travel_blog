1c397e3292d61bd4a18ef3e14cc3dc7b38e9d488
Fabliha Hossain <fablihahossain27@gmail.com>
2020-08-16 19:33:29 -0400

    updated admin page with comments

--- travelBlog/travelEntries/admin.py
+++ travelBlog/travelEntries/admin.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from .models import User, Entry, EntryImage
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
+# User Details
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
 	list_display = ['name', 'username', 'email', 'role']
 
+# Details for Entry Images
 class ArrayModelAdmin(admin.ModelAdmin, DynamicArrayMixin):
     list_display = ['title', 'author', 'descriptions']
 admin.site.register(Entry, ArrayModelAdmin)
 
+# Entry Details
 @admin.register(EntryImage)
 class EntryImages(admin.ModelAdmin):
 	list_display = ['entry', 'image']
