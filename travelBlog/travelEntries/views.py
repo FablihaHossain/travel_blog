284b80bb80cec37698a2add4ad493c095cf6113d
Fabliha Hossain <fablihahossain27@gmail.com>
2020-08-16 19:33:10 -0400

    updatedviews with comments

--- travelBlog/travelEntries/views.py
+++ travelBlog/travelEntries/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import User, Entry, EntryImage
 
+# Basic Route for the Site
 def index(request):
 	# Getting all users
 	users = User.objects.all()
@@ -13,8 +14,10 @@ def index(request):
 
 	return render(request, 'index.html', {'users':users, 'entries':entries, 'images':images})
 
+# Login Page
 def login(request):
 	return render(request, 'login.html')
 
+# Registration Page
 def register(request):
 	return render(request, 'registration.html')
