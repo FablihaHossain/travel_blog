a1135adbecf32c0a808ab81bdfd108f4e4e4892e
Fabliha Hossain <fablihahossain27@gmail.com>
2020-08-15 21:19:36 -0400

    updated models to work with db connection

--- travelBlog/travelEntries/models.py
+++ travelBlog/travelEntries/models.py
@@ -8,23 +8,20 @@ class User(models.Model):
 	email = models.EmailField(blank = False)
 	password = models.CharField(max_length = 300, blank = False)
 	role = models.CharField(max_length = 20, choices = ROLES, blank = False)
-	# Users can have multiple entries in the blog site
-	#entries = models.ManyToManyField('Entry', blank = True)
 
 	# Print statement for Python Shell
 	def __str__(self):
-		return self.name
+		return self.username
 
 class Entry(models.Model):
 	title = models.CharField(max_length = 400, blank = False)
 	author = models.ForeignKey('User', on_delete=models.CASCADE)
-	#descriptions = ArrayField(models.charField(max_length = 500), size = 5)
-	#images = ArrayField(models.ImageField(upload_to(user_entry_pathname)), size = 5)
+	descriptions = ArrayField(models.CharField(max_length = 500), size = 5)
+	# Attempting to create a new path file for each entry
+	pathname = 'entryImages/%s/%s' % (author, title)
+	images = ArrayField(models.ImageField(upload_to = pathname), size = 5)
 
 	# Print statement for Python Shell	
 	def __str__(self):
 		return self.title
-
-def user_entry_pathname(instance, filename):
-	return 'user{0}/{1}'.format(instance.user.id, filename)
 # Credit to https://docs.djangoproject.com/en/3.1/ref/contrib/postgres/fields/#arrayfield
