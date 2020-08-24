b8ff43424709f22eddf15205374977a74b893c65
Fabliha Hossain <fablihahossain27@gmail.com>
2020-08-24 15:40:23 -0400

    created two new views to access new pages

--- travelBlog/travelEntries/views.py
+++ travelBlog/travelEntries/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import User, Entry, EntryImage
+from django.http import Http404
 
 # Basic Route for the Site
 def index(request):
@@ -21,3 +22,14 @@ def login(request):
 # Registration Page
 def register(request):
 	return render(request, 'registration.html')
+
+def newEntry(request):
+	return render(request, 'newEntry.html')
+
+def viewEntry(request, entry_id):
+	try:
+		entry = Entry.objects.get(id = entry_id)
+		images = EntryImage.objects.all()
+	except Entry.DoesNotExist:
+		raise Http404(f'Entry with id {entry_id} does not exist')
+	return render(request, 'viewEntry.html', {'entry':entry,'images':images})
