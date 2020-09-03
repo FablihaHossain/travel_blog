efd52c04028b35fb62e76a7ce3e7f069acb95408
Fabliha Hossain <fablihahossain27@gmail.com>
2020-09-02 20:49:55 -0400

    created a new form for new entry

--- travelBlog/travelEntries/forms.py
+++ travelBlog/travelEntries/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import User
+from .models import User, Entry, EntryImage
+from django.contrib.postgres.forms import SimpleArrayField
 
 class LoginForm(forms.Form):
 	username = forms.CharField(label="username", max_length=100)
@@ -12,9 +13,19 @@ class RegistrationForm(forms.ModelForm):
 		widgets = {'password':forms.PasswordInput, 'role':forms.HiddenInput}
 		#role = forms.CharField(widget=forms.HiddenInput())
 
+class NewEntryForm(forms.ModelForm):
+	class Meta:
+		model = Entry
+		fields = ['title', 'author', 'numOfDescriptions', 'descriptions']
+	# descriptions will be separated based on commas
+	descriptions = SimpleArrayField(forms.CharField(max_length=100))
+
+
 # extra notes
 # example = ChoiceField(label="Example", choices = [('1'), ('2')....])
 # widget = forms.Textarea
 # image = forms.ImageField() => add request.FILES alongside request.POST in views.py
 
 # Note to self: Perhaps add another field into Entry model that keeps track of how many descriptions there will be
+
+# Credit to https://stackoverflow.com/questions/56218408/how-to-define-arrayfield-to-django-forms
