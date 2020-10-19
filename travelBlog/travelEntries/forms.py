331ee6cc780c3babdb276bf12525130609cdb0b4
Fabliha Hossain <fablihahossain27@gmail.com>
2020-10-19 19:48:00 -0400

    updated entry form details to fix bug

--- travelBlog/travelEntries/forms.py
+++ travelBlog/travelEntries/forms.py
@@ -14,20 +14,23 @@ class RegistrationForm(forms.ModelForm):
 		#role = forms.CharField(widget=forms.HiddenInput())
 
 class NewEntryForm(forms.ModelForm):
+	numOfDescriptions = forms.CharField(label='numOfDescriptions', widget=forms.TextInput(attrs={'min':1,'max': '5','type': 'number'}))
+	# Descriptions will be separated based on commas
+	descriptions = SimpleArrayField(forms.CharField(max_length=100))
 	class Meta:
 		model = Entry
 		fields = ['title', 'author', 'numOfDescriptions', 'descriptions']
-	# Descriptions will be separated based on commas
-	descriptions = SimpleArrayField(forms.CharField(max_length=100))
 
 class EntryImageForm(forms.ModelForm):
 	class Meta:
 		model = EntryImage
-		fields = ['entry', 'image']
-		widgets = {'image':forms.ImageField}
+		fields = ['image']
+	image = forms.ImageField(label='image')
+		# widgets = {'image':forms.ImageField}
 # extra notes
 # example = ChoiceField(label="Example", choices = [('1'), ('2')....])
 # widget = forms.Textarea
 # image = forms.ImageField() => add request.FILES alongside request.POST in views.py
 
 # Credit to https://stackoverflow.com/questions/56218408/how-to-define-arrayfield-to-django-forms
+# Credit to https://stackoverflow.com/questions/37024650/specify-max-in-numberinput-widget
